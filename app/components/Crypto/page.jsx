7b4a9a0d953c90514d6e5e3d7b08c1ce94fd1a83
Shookrun <sukranmamedov999@gmail.com>
2024-10-09 20:20:14 +0400

    Crypto Finished

--- app/components/Crypto/page.jsx
+++ app/components/Crypto/page.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const Crypto = () => {
   return (
-    <div>Crypto</div>
-  )
-}
+    <div className="flex  justify-center items-center">
+      <div className="w-[40%] flex flex-col justify-center items-center gap-4">
+      <h2 className="text-center text-6xl tracking-wide leading-relaxed">Balancing <br />Crypto Markets </h2>
+      <p>Our fully automated proprietary quantitative trading software provides 24/7 liquidity to 170+ crypto assets across 25+ centralized spot and derivative crypto exchanges.</p>
+      <button className=" bg-gradient-to-r from-[#665DCD] from-3% via-[#5FA4E6] via-30% to-[#D2AB67] to-90% py-3 px-4">GET IN TOUCH</button>
+      </div>
+    </div>
+  );
+};
 
-export default Crypto
+export default Crypto;
--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Crypto from "./components/Crypto/page";
 
 export default function Home() {
   return (
     <div>
-      
+      <Crypto/>
     </div>
   );
 }
