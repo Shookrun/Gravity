20444fc115e3d082fe1a288f16a74ed2a631d89e
Shookrun <sukranmamedov999@gmail.com>
2024-10-09 20:21:10 +0400

    GravityTeam created

---
+++ app/components/GravityTeam/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const GravityTeam = () => {
+  return (
+    <div>GravityTeam</div>
+  )
+}
+
+export default GravityTeam
--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Crypto from "./components/Crypto/page";
+import GravityTeam from "./components/GravityTeam/page";
 
 export default function Home() {
   return (
     <div>
       <Crypto/>
+      <GravityTeam/>
     </div>
   );
 }
