8db5137d0228e250414c5d72f5490aa9c97a1ce7
Shookrun <sukranmamedov999@gmail.com>
2024-10-09 20:23:48 +0400

    part1 finished

--- app/components/GravityTeam/page.jsx
+++ app/components/GravityTeam/page.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const GravityTeam = () => {
   return (
-    <div>GravityTeam</div>
-  )
-}
+    <div className="flex justify-center items-center mt-20">
+      <div>
+        <div className="flex  flex-col items-center gap-6 justify-center">
+          <h2 className="text-4xl">About Gravity Team</h2>
+          <p className="text-center w-[40%]">
+            At Gravity Team, we are on the mission to balance the supply and
+            demand across crypto markets worldwide. We are a crypto native
+            market maker founded by traders, developers, and innovators who are
+            strong believers and supporters of the future of decentralization
+            and digital assets.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default GravityTeam
+export default GravityTeam;
