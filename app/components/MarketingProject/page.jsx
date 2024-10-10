46e83a4bd494affd95c031ab1408e1ecbac36e98
Shookrun <sukranmamedov999@gmail.com>
2024-10-10 19:53:34 +0400

    react-icons installed

--- app/components/MarketingProject/page.jsx
+++ app/components/MarketingProject/page.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import { FaArrowRight } from "react-icons/fa";
 
 const MarketingProject = () => {
   return (
-    <div>
+    <div className='flex'>
         <div>
-
+    <h2 className='text-2xl'>Market Making for Crypto Projects</h2>
+    <p className='font-bold'>Accelerate your token’s journey by boosting its liquidity</p>
+    <p>We invest in building long-term, sustainable relationships and support our projects in their growth journey with our services, industry expertise and network.</p>
         </div>
         <div>
-            
+
         </div>
     </div>
   )
