a5469000fcb2efc42d439ee84be70ab3beac0548
Shookrun <sukranmamedov999@gmail.com>
2024-10-10 19:50:17 +0400

    CryptoMarketing Finished

--- app/components/CryptoMarketing/page.jsx
+++ app/components/CryptoMarketing/page.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const CrypoMarketing = () => {
   return (
-    <div>CrypoMarketing</div>
-  )
-}
+    <div className="flex flex-col justify-center gap-8 mt-20 items-center">
+      <h2 className="text-4xl">Crypto Market Making</h2>
+      <p className="w-[30%] text-center">
+        We are a global crypto liquidity provider and algorithmic market maker.
+        We trade digital assets listed on Centralized Exchanges in over 15
+        countries worldwide.
+      </p>
+    </div>
+  );
+};
 
-export default CrypoMarketing
+export default CrypoMarketing;
